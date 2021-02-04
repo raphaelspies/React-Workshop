73e4987538f0f89052505f9482354073258a6073
uncosung <eric.k.sung@gmail.com>
2021-02-04 13:58:45 -0800

    Updated server

--- public/server/index.js
+++ public/server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-const cors = require('cors');
 const port = 3001;
 
 server.use(express.static(path.join(__dirname, '../../client/dist')));
